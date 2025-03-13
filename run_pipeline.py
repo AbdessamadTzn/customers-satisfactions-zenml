f44be116864993cda6d0d3640b91fd269c355004
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2025-03-13 15:26:40 +0100

    Run Piepline

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+from pipelines.train_pipeline import training_pipeline
+
+if __name__ == "__main__":
+    #Run the pipeline
+    training_pipeline(data_path='C:\\Users\\oo\Desktop\\Github DEV\\mlflow\\data\\olist_customers_dataset.csv')
+
+
